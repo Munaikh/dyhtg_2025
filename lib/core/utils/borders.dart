5265f4239a15b7ae3528817a581be61edd9f9390
“Mansoor <2961033a@student.gla.ac.uk>
2025-10-25 12:04:21 +0100

    refactor project structure and update README for clarity

---
+++ lib/core/utils/borders.dart
@@ -0,0 +1,8 @@
+class AppBorders {
+  AppBorders._();
+
+  static const double small = 12;
+  static const double medium = 16;
+  static const double large = 20;
+  static const double xlarge = 24;
+}
---
+++ lib/core/utils/spacing.dart
@@ -0,0 +1,9 @@
+class AppSpacing {
+  AppSpacing._();
+
+  static const double small = 8;
+  static const double medium = 12;
+  static const double large = 16;
+  static const double xlarge = 24;
+  static const double xxlarge = 32;
+}
